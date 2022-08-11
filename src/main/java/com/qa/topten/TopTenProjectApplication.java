2b189819196753bc72fe6fd60fd69cbdaa052475
Dan Harrison <danhharrison5@gmail.com>
2022-08-11 10:21:10 +0100

    initial project commit

---
+++ src/main/java/com/qa/topten/TopTenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.topten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopTenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopTenProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/topten/TopTenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.topten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopTenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
